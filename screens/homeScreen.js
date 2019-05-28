81faee866dd33e6be4f6965f1ea421d1b6062234
Gaiish <gdishim@gmail.com>
2019-05-28 19:03:50 +0300

    added list of images on the home screen

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,10 +1,56 @@
 import React from "react";
-import { Container, Content, Text } from "native-base";
+import { Container, Content, Text, Card, CardItem } from "native-base";
+import { FlatList, Image } from "react-native";
 
-export default () => (
-  <Container>
-    <Content>
-      <Text>Home</Text>
-    </Content>
-  </Container>
-);
+import firebase from "react-native-firebase";
+
+export default class extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    let db = firebase.firestore();
+
+    //here we are going to get all the documents
+    db.collection("places")
+      .get()
+      .then(snapshot => {
+        let data = [];
+
+        snapshot.forEach(doc => {
+          data = [...data, { ...doc.data(), id: doc.id }];
+          this.setState({ data: data });
+        });
+      });
+  }
+
+  render() {
+    let { data } = this.state;
+    return (
+      <Container>
+        <Content>
+          <FlatList
+            data={data}
+            keyExtractor={(item, index) => item.id}
+            renderItem={({ item }) => (
+              <Card>
+                <CardItem cardBody>
+                  <Image
+                    source={{
+                      uri: item.img_url
+                    }}
+                    style={{ height: 200, width: null, flex: 1 }}
+                  />
+                </CardItem>
+                <CardItem>
+                  <Text>{item.name}</Text>
+                </CardItem>
+              </Card>
+            )}
+          />
+        </Content>
+      </Container>
+    );
+  }
+}
